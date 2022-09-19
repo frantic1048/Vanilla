f37370ca25446c8eac22635bc1871a725758ab1b
frantic1048 <i@frantic1048.com>
2022-09-19 17:58:35 +0800

    no tabs

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,5 +1,7 @@
 return {
     default_prog = { '/usr/local/bin/elvish' },
-    color_scheme = 'Tango (base16)',
-    window_background_opacity = 0.6,
+    color_scheme = 'zenwritten_dark',
+    window_background_opacity = 0.4,
+    enable_tab_bar = false,
+    hide_tab_bar_if_only_one_tab = true
 }

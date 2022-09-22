d5948865a5ad387bb10af6c324158f1b14c44339
frantic1048 <i@frantic1048.com>
2022-09-22 19:01:13 +0800

    play with JetBrains Mono

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,6 +1,9 @@
+local wezterm = require 'wezterm'
+
 return {
     default_prog = { '/usr/local/bin/elvish' },
     color_scheme = 'zenwritten_dark',
+    font = wezterm.font 'JetBrains Mono',
     window_background_opacity = 0.4,
     enable_tab_bar = false,
     hide_tab_bar_if_only_one_tab = true
